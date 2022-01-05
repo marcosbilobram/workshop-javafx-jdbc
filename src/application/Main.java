754199e3998c9a4512e34e6cfb96c74d06a7277b
Marcos Henrique <marcosbilobram@hotmail.com>
2022-01-05 17:23:44 -0300

    About view

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,24 +9,31 @@ import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+	
+	private static Scene mainScene;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
-			ScrollPane scrollpane = loader.load();
+			ScrollPane scrollPane = loader.load();
 			
-			scrollpane.setFitToHeight(true);
-			scrollpane.setFitToWidth(true);
+			scrollPane.setFitToHeight(true);
+			scrollPane.setFitToWidth(true);
 			
-			Scene mainScene = new Scene(scrollpane);
+			mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX application");
-			primaryStage.show();
+			primaryStage.show();	
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
+	public static Scene getMainScene() {
+		return mainScene;
+	}
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,11 +1,20 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 public class MainViewController implements Initializable {
 	
@@ -30,7 +39,7 @@ public class MainViewController implements Initializable {
 	
 	@FXML
 	public void onMenuItemAboutAction() {
-		System.out.println("onMenuItemSAboutAction");
+		loadView("/gui/About.fxml");
 	}
 	
 	@Override
@@ -39,5 +48,22 @@ public class MainViewController implements Initializable {
 		
 	}
 	
-	
+	private synchronized void loadView(String absoluteName) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newVbox = loader.load();
+			
+			Scene mainScene = Main.getMainScene();
+			VBox mainVBox = (VBox)((ScrollPane) mainScene.getRoot()).getContent();
+			
+			Node mainMenu = mainVBox.getChildren().get(0);
+			mainVBox.getChildren().clear();
+			mainVBox.getChildren().add(mainMenu);
+			mainVBox.getChildren().addAll(newVbox.getChildren());
+			
+		}
+		catch (IOException e) {
+			Alerts.showAlert("IO Exception", "Erroe loading view", e.getMessage(), AlertType.ERROR);
+		}
+	}
 }
